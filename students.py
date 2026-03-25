e257d87a214fabd6edf9ddf3921263d55308acc7
5h1ro0o <120198629+5h1ro0o@users.noreply.github.com>
2026-03-25 12:36:13 +0100

    troisième question

--- students.py
+++ students.py
@@ -25,46 +25,18 @@ class SchoolClass:
     def add_student(self, student: Student):
         self.students.append(student)
 
-    @property
-    def students(self):
-        return self.__students
-
-class Student:
-
-    def __init__(self, name: str, grade1: float, grade2: float, grade3: float):
-        self.__name = name
-        self.__grades = [grade1, grade2, grade3]
-
-    @property
-    def name(self):
-        return self.__name
-
-    @property
-    def grades(self):
-        return self.__grades
-
-    @property
-    def average(self):
-        return sum(self.__grades) / len(self.__grades)
-
-
-class SchoolClass:
-
-    def __init__(self):
-        self.__students = []
-
-    def add_student(self, student: Student):
-        self.__students.append(student)
+    def rank_matter_1(self):
+        return sorted(self.students, key=lambda s: s.grades[0], reverse=True)
 
     def rank_by_subject(self, subject_index: int):
-        return sorted(self.__students, key=lambda s: s.grades[subject_index], reverse=True)
+        return sorted(self.students, key=lambda s: s.grades[subject_index], reverse=True)
 
     def rank_by_average(self):
-        return sorted(self.__students, key=lambda s: s.average, reverse=True)
+        return sorted(self.students, key=lambda s: s.average, reverse=True)
 
     @property
     def students(self):
-        return self.__students
+        return self.students
 
 
 if __name__ == '__main__':
@@ -72,6 +44,10 @@ if __name__ == '__main__':
     school_class.add_student(Student('J', 10, 12, 13))
     school_class.add_student(Student('A', 8, 2, 17))
     school_class.add_student(Student('V', 9, 14, 14))
+
+    print('\nClassement Matière 1:')
+    for student in school_class.rank_matter_1():
+        print(f'  {student.name}: {student.grades[0]}')
 
     for i, subject in enumerate(['Matière 1', 'Matière 2', 'Matière 3']):
         print(f'\nClassement {subject}:')
